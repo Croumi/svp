77f54c26c83183c6f37f67738cfb4eb71d93e85e
Gilles <thia.gilles@gmail.com>
2020-12-16 07:15:06 +0100

    add add_gaussian_noise() and swap_coordinates()

--- ano.py
+++ ano.py
@@ -13,6 +13,7 @@ import utility_POI
 import utility_POI_perWeek
 import utility_tuile
 import sqlite3
+import copy
 
 def get_config():
     with open('config.yml') as f:
@@ -29,6 +30,9 @@ class Row:
     def __str__(self):
         return('\t'.join([self.user_id, str(self.date), str(self.lattitude), str(self.longitude)]))
 
+    def get_values(self):
+        return (self.user_id, self.date.year, self.date.month, self.date.day, self.date.hour, self.date.minute, self.date.second, self.lattitude, self.longitude)
+
 class Database:
     def __init__(self, config):
         self.config = config
@@ -37,7 +41,7 @@ class Database:
 
         self.c.execute('''SELECT name FROM sqlite_master WHERE type='table' AND name='svp';''')
         if self.c.fetchone() == None:
-            self.c.execute('''CREATE TABLE svp (user_id, year, month, day, hour, min, second, lattitude, longitude)''')
+            self.c.execute('''CREATE TABLE svp (user_id, year, month, day, hour, minute, second, lattitude, longitude)''')
             self.fill()
 
     def fill(self):
@@ -61,8 +65,8 @@ class Database:
         self.conn.commit()
 
     def print(self):
-        for row in self.c.execute('SELECT * FROM svp'):
-            print(row)
+        for (index, row) in enumerate(self.c.execute('SELECT * FROM svp')):
+            print(index, row)
 
     def write(self):
         fout = open(self.config['ano_file'],"w")
@@ -75,6 +79,9 @@ class Database:
     def close(self):
         self.conn.close()
 
+    def save(self):
+        self.conn.commit()
+
     def get_number_of_rows(self):
         self.c.execute('SELECT COUNT(*) from svp')
         return self.c.fetchone()[0]
@@ -82,13 +89,36 @@ class Database:
     def run_query(self, query):
         return self.c.execute(query)
 
-def swap_coordinates(row1, row2):
-    row1.lattitude, row2.lattitude = row2.lattitude, row1.lattitude
-    row1.longitude, row2.longitude = row2.longitude, row1.longitude
+    def update(self, row_before, row_after):
+        self.conn.cursor().execute("UPDATE svp SET user_id = ?, year = ?, month = ?, day = ?, hour = ?, minute = ?, second = ?, lattitude = ?, longitude = ? WHERE user_id = ? AND year = ? AND month = ? AND day = ? AND hour = ? AND minute = ? AND second = ? AND lattitude = ? AND longitude = ?", row_after.get_values() + row_before.get_values())
 
-def add_gaussian_noise(row, sigma):
-    row.lattitude += random.gauss(0, sigma)
-    row.longitude += random.gauss(0, sigma)
+    def add_gaussian_noise(self, row, sigma):
+        old_row = Row(*row)
+        new_row = copy.deepcopy(old_row)
+
+        new_row.lattitude += random.gauss(0, sigma)
+        new_row.longitude += random.gauss(0, sigma)
+
+        self.update(old_row, new_row)
+
+    def swap_coordinates(self, row1, row2):
+        old_row1 = Row(*row1)
+        new_row1 = copy.deepcopy(old_row1)
+        old_row2 = Row(*row2)
+        new_row2 = copy.deepcopy(old_row2)
+
+        new_row1.lattitude, new_row2.lattitude = old_row2.lattitude, old_row1.lattitude
+        new_row1.longitude, new_row2.longitude = old_row2.longitude, old_row1.longitude
+
+        self.update(old_row1, new_row1)
+        self.update(old_row2, new_row2)
+
+def anonymise(db):
+    for row in db.run_query('SELECT * FROM svp'):
+        db.add_gaussian_noise(row, 0.01)
+
+    rows = db.run_query('SELECT * FROM svp LIMIT(2)').fetchall()
+    db.swap_coordinates(rows[0], rows[1])
 
 def run_utility_metrics(config):
     original_file = config['original_file']
@@ -108,11 +138,8 @@ def run_utility_metrics(config):
 def main():
     config = get_config()
     db = Database(config)
-    #db.print()
-    #db.write()
-    for row in db.run_query('SELECT * FROM svp'):
-        print(row)
-        
+    anonymise(db)
+    db.write()
     run_utility_metrics(config)
 
 if __name__ == "__main__":
